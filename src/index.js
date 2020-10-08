ecf06fc566ee2d2fa0aa8ca33b0d2f0939aea4b5
AfterMetheFlood <AfterMetheFlood@outllok.com>
2020-10-08 01:01:45 -0500

    test git

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import _ from "lodash";
 function component() {
   const element = document.createElement("div");
 
-  element.innerHTML = _.join(["Hello", "News"], " ");
+  element.innerHTML = _.join(["Hello ", "News"], " ");
 
   return element;
 }
