abd38054e2b3a13b652e3a6a3c34d969bee382e1
AfterMetheFlood <AfterMetheFlood@outllok.com>
2020-10-23 18:15:15 -0500

    add Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { TabView, TabPanel } from "primereact/tabview";
 import Table from "./Table";
+import Page from "./Page";
 import { get9to5mac, getCnBeta } from "./feed";
 
 class App extends Component {
@@ -30,7 +31,14 @@ class App extends Component {
         onTabChange={(e) => this.setState({ activeIndex: e.index })}
       >
         <TabPanel header="9to5mac">
-          <Table data={_9to5mac} />
+          <div className="p-grid">
+            <div className="p-col-4">
+              <Table data={_9to5mac} />
+            </div>
+            <div className="p-col">
+              <Page />
+            </div>
+          </div>
         </TabPanel>
         <TabPanel header="cnBeta">
           <Table data={cnBeta} />
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import 'primereact/resources/themes/saga-blue/theme.css';
 import 'primereact/resources/primereact.min.css';
+import 'primeflex/primeflex.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
