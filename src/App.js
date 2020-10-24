f940101954e7d1622146798c0a36cb947cbe6a16
AfterMetheFlood <AfterMetheFlood@outllok.com>
2020-10-23 20:26:44 -0500

    add ithome

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { TabView, TabPanel } from "primereact/tabview";
 import Table from "./Table";
 import Page from "./Page";
-import { get9to5mac, getCnBeta } from "./feed";
+import { getITHome, get9to5mac, getCnBeta } from "./feed";
 
 class App extends Component {
   state = {
@@ -11,6 +11,11 @@ class App extends Component {
   };
 
   componentDidMount() {
+    getITHome().then((data) => {
+      this.setState({
+        ithome: data,
+      });
+    });
     get9to5mac().then((data) => {
       this.setState({
         _9to5mac: data,
@@ -32,12 +37,24 @@ class App extends Component {
   render() {
     const cnBeta = this.state.cnBeta ? this.state.cnBeta : {};
     const _9to5mac = this.state._9to5mac ? this.state._9to5mac : {};
+    const ithome = this.state.ithome ? this.state.ithome : {};
     const article = this.state.selectedArticle;
     return (
       <TabView
         activeIndex={this.state.activeIndex}
         onTabChange={(e) => this.setState({ activeIndex: e.index })}
       >
+        <TabPanel header="ithome">
+          <div className="p-grid">
+            <div className="p-col-4">
+              <Table data={ithome} selectArticle={this.selectArticle} />
+            </div>
+            <div className="p-col-fixed">
+              <Page article={article} />
+            </div>
+          </div>
+        </TabPanel>
+
         <TabPanel header="9to5mac">
           <div className="p-grid">
             <div className="p-col-4">
@@ -48,6 +65,7 @@ class App extends Component {
             </div>
           </div>
         </TabPanel>
+
         <TabPanel header="cnBeta">
           <div className="p-grid">
             <div className="p-col-4">
@@ -58,7 +76,6 @@ class App extends Component {
             </div>
           </div>
         </TabPanel>
-        <TabPanel header="Header III">Content III</TabPanel>
       </TabView>
     );
   }
--- src/feed.js
+++ src/feed.js
@@ -3,8 +3,15 @@ import Parser from "rss-parser";
 const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
 const RSS_URL_9TO5MAC = "https://9to5mac.com/feed/";
 const RSS_URL_CNBETA = "https://www.cnbeta.com/backend.php";
+const RSS_URL_ITHOME = "https://www.ithome.com/rss/";
 
 let parser = new Parser();
+
+export async function getITHome() {
+  const feed = await parser.parseURL(CORS_PROXY + RSS_URL_ITHOME);
+  console.log(feed);
+  return feed;
+}
 
 export async function get9to5mac() {
   const feed = await parser.parseURL(CORS_PROXY + RSS_URL_9TO5MAC);
